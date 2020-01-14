eea4ac985c10141f25efdf40623f422780033aa7
Kirill Onufrienko <kovdor@mail.ru>
2020-01-14 23:04:52 +0300

    working sass + from library

--- test-example/example-component.tsx
+++ test-example/example-component.tsx
@@ -3,7 +3,7 @@ import { getCurrentTimeMillis } from 'test_bazel_build/test-library';
 
 export class TestComponent extends React.Component {
     render() {
-        return <button onClick={this.buttonClick} className="example-buton">Push me</button>;
+        return <button onClick={this.buttonClick} className="example-button">Push me</button>;
     }
 
     private buttonClick = () => {
--- test-web/app.tsx
+++ test-web/app.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
 import { TestComponent } from 'test_bazel_build/test-example';
 
-const app = <TestComponent />;
-
-export default app;
+export default <TestComponent />;
